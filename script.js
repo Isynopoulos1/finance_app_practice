4cb96b0821575610578515af8b60949b327cc8d5
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-12-05 19:57:42 +0100

    adding query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//selecting elements
+const totalEl = document.querySelector(".euro");
+const savings = document.querySelector("#savings");
+const expences = document.querySelector("#expences");
+const concept = document.querySelector("#concept");
