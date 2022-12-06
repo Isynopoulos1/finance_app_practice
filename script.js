57d10263ec01bb935632f1265115eec039cc2955
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-12-06 10:52:32 +0100

    stablish initil values in script

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 "use strict";
 
 //selecting elements
-const totalEl = document.querySelector(".euro");
-const savings = document.querySelector("#savings");
-const expences = document.querySelector("#expences");
-const concept = document.querySelector("#concept");
+const totalEl = document.querySelector("#total");
+const savingsEl = document.querySelector("#savings");
+const amountSavedEl = document.querySelector(".amount-saved");
+const expencesEl = document.querySelector("#expences");
+const amountSpendedEl = document.querySelector(".amount-spended");
+const conceptEl = document.querySelector("#concept");
+const detailsConceptEl = document.querySelector("#concept-amount");
+const conceptTransactionEl = document.querySelector(".concept-transaccion");
+const transactionInputed = document.querySelector("#write-concept");
+const amounTransationEl = document.querySelector(".amount-transaction");
+const btnTransactionEl = document.querySelector("button");
+
+//stablish initial amounts
+
+const totalBudget = 0;
+const incomeValue = 0;
+const expencesValue = 0;
+const summaryValues = 0;
