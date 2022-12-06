8bea5c56710632bb9b56e23830f73c033683b75e
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-12-06 12:39:00 +0100

    test

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const savingsEl = document.querySelector("#savings");
 const amountSavedEl = document.querySelector(".amount-saved");
 const expencesEl = document.querySelector("#expences");
 const amountSpendedEl = document.querySelector(".amount-spended");
+const summaryDetailsEl = document.querySelector(".summary-container");
 const conceptEl = document.querySelector("#concept");
 const detailsConceptEl = document.querySelector("#concept-amount");
 const conceptTransactionEl = document.querySelector(".concept-transaccion");
@@ -13,9 +14,22 @@ const transactionInputed = document.querySelector("#write-concept");
 const amounTransationEl = document.querySelector(".amount-transaction");
 const btnTransactionEl = document.querySelector("button");
 
+// setting a global variable to this keys and then storing the values inside the function
+let totalBudget, expencesValue, incomeValue, summaryValues;
+
 //stablish initial amounts
 
-const totalBudget = 0;
-const incomeValue = 0;
-const expencesValue = 0;
-const summaryValues = 0;
+const displayedBudget = () => {
+  expencesValue = 0;
+  totalBudget = 0;
+  incomeValue = 0;
+  summaryValues = 0;
+
+  amountSavedEl.textContent = 0;
+  amountSpendedEl.textContent = 0;
+  totalEl.textContent = 0;
+
+  return summaryDetailsEl.classList.add("hide");
+};
+
+displayedBudget();
