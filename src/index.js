c1bd036a834b6f2ddde68e47c46f74a94e530c44
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-12-08 16:41:32 +0100

    creating the life cycle, handle function and render functions

--- src/index.js
+++ src/index.js
@@ -1,55 +1,74 @@
-// VARIABLES
+// NOTE VARIABLES
 const addTransactionButton = document.getElementById("add-transaction");
 const transactionList = document.getElementById("transactions");
 const concept = document.getElementById("concept");
 const amount = document.getElementById("amount");
+const transactionsList = [];
+let total = 0;
+let totalExpenses = 0;
+let totalIncome = 0;
 
-// LIFECYLE
+// NOTE LIFECYLE
 addTransactionButton.addEventListener("click", (e) => {
   e.preventDefault();
 
   // CHECK IF NOT EMPTY
   if (concept.value !== "" && amount.value !== "") {
-    renderTransaction(concept.value, amount.value);
-    handleSaveTransaction();
+    const type = Number(amount.value) < 0 ? "TYPE_EXPENSE" : "TYPE_INCOME";
+    handleUpdate(concept.value, amount.value, type);
     handleReset(concept);
-    return handleReset(amount);
+    handleReset(amount);
+    return handleSaveTransaction();
   } else {
     return console.error("amount or concept cannot be empty");
   }
 });
 // TODO EVENT LISTENNER OF DELETE TRANSACTION BUTTON
 
-// HANDLE FUNCTIONS
-const loadSavedTransactions = () => {
-  console.log("loading transactions");
-  // TODO LOAD PREVIOUSLY SAVED IN LOCALSTORAGE TRANSACTIONS
+// NOTE HANDLE FUNCTIONS
+const loadSavedTransactions = () => console.log("loading transactions");
+const handleSaveTransaction = () => console.log("save transaction");
+const handleDelete = (node) => node.remove();
+const handleReset = (node) => (node.value = "");
+const handleTotal = (list) => {
+  return list.reduce((pre, acc) => Number(pre) + Number(acc.amount), 0);
 };
-const handleSaveTransaction = () => {
-  console.log("save transaction");
-  // TODO SAVE TRANSACTION TO LOCALSTORAGE
-  return;
-};
-const handleReset = (node) => {
-  node.value = "";
-};
-const handleDelete = (node) => {
-  node.remove();
+const handleUpdate = (concept, amount, type) => {
+  const container = document.querySelector(".transaction-container");
+  if (container) handleDelete(container);
+  transactionsList.push({ concept: concept, amount: amount, type: type });
+  total = handleTotal(transactionsList);
+  totalExpenses = handleTotal(
+    transactionsList.filter((i) => i.type === "TYPE_INCOME")
+  );
+  totalIncome = handleTotal(
+    transactionsList.filter((i) => i.type === "TYPE_EXPENSE")
+  );
+
+  return renderTransactionList(transactionsList);
 };
 
-// RENDER FUNCTIONS
-const renderTransaction = (concept, amount) => {
-  // GENERATE ELEMENT
+// NOTE RENDER FUNCTIONS
+const renderTransactionItem = (concept, amount, id) => {
   const transaction = document.createElement("div");
-  // TODO ADD AN ID TO THE TRANSACTION
+  transaction.setAttribute("id", id);
+  transaction.setAttribute("class", "transaction-item");
   const transactionContent = `
     <p>${concept}</p>
     <p>$${amount}</p>
   `;
   transaction.innerHTML = transactionContent;
-  // APPEND TO THE LIST
-  return transactionList.appendChild(transaction);
+  return transaction;
+};
+const renderTransactionList = (list) => {
+  const listContainer = document.createElement("div");
+  listContainer.setAttribute("class", "transaction-container");
+  for (let i = 0; i < list.length; i++) {
+    const children = renderTransactionItem(list[i].concept, list[i].amount, i);
+    listContainer.appendChild(children);
+  }
+  return transactionList.appendChild(listContainer);
 };
 
-// INVOCTIONS
+// NOTE INVOCTIONS
 loadSavedTransactions();
